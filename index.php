b5acbc4e7fa5268e0cb2ec87f50996f9a3e17422
Daniel Wiegand <daniel@wiegand.io>
2019-05-23 11:06:57 +0200

    added some basic content

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
                 <li>
                     <a href="tel:+491704706439" rel="noopener noreferrer">
                         <svg class="icon icon-phone"><use xlink:href="images/symbol-defs.svg#icon-phone"></use></svg> +49 170 470 6439
-                    </a>
+                    </a>&nbsp;
                 </li>
                 <li>
                     <a href="mailto:daniel@wiegand.io" target="_blank" rel="noopener noreferrer">
@@ -48,7 +48,7 @@
                 </li>
             </ul>
 
-            <ul>
+            <ul class="social">
                 <li>
                     <a href="https://www.linkedin.com/in/wiegimania/" target="_blank" rel="noopener nofollow">
                         <svg class="icon icon-linkedin" aria-label="Daniel Wiegand on LinkedIn"><use xlink:href="images/symbol-defs.svg#icon-linkedin"></use></svg>
@@ -88,12 +88,76 @@
             <h1>Daniel Wiegand<br><span>Creative Technologist. Adventurer. Consultant. Coach. Speaker.</span></h1>
         </section>
 
-        <section class="orange">
-            <h2>Meine Leistungen</h2>
+        <section>
+            <h2>Über mich</h2>
+            <p>
+                Mein Name ist Daniel Wiegand. Seit 2016 arbeite ich freiberuflich in den Bereichen Consulting und Entwicklung, mit den Schwerpunkten Frontend und WordPress.
+            </p>
+            <p>
+                Vor Gründung des Creative Studio Daniel Wiegand war ich knapp 10 Jahre Geschäftsführender Gesellschafter / Technischer Leiter der FirstSquare GmbH, eine der führenden Digitalagenturen aus Karlsruhe.
+            </p>
+            <h3>Leistungen</h3>
+            <p>
+                Sie sind auf der Suche nach einem ganzheitlichen technischen Partner &ndash; von der Beratung, über die Konzeption bis zur Umsetzung? Durch meine langjährige Erfahrung habe ich ein hohes Expertenwissen in den unterschiedlichen Bereichen. Ein Auszug meiner Leistungen finden Sie hier:<br>
+            </p>
+            <ul>
+                <li>Responsive Frontends</li>
+                <li>Accessibility</li>
+                <li>WordPress-Entwicklung & Systembetreuung</li>
+                <li>Performance Audits</li>
+                <li>Workshops</li>
+                <li>uvm.</li>
+            </ul>
+            <p>
+                <a href="#contact">Sprechen Sie mich an</a>.
+            </p>
         </section>
 
         <section>
-            <h2>Meine Kunden</h2>
+            <h2>Meine Kunden & Partner</h2>
+            <ul class="logo">
+                <li><img src="images/Smashing-Magazine-Logo.png" alt="Logo Smashing Magazine" /></li>
+                <li><img src="images/AWO-Logo.png" alt="Logo AWO" style="height:60px;width: auto;" /></li>
+                <li><img src="images/Chrono24-Logo.png" alt="Logo Chrono24" /></li>
+                <li><img src="images/Theater-Baden-Baden-Logo.png" alt="Logo Theater Baden-Baden" /></li>
+                <li><img src="images/NORDKAP-Logo.png" alt="Logo Werbeagentur NORD|KAP" /></li>
+                <li><img src="images/Schlau-Werkstatt-Logo.png" alt="Logo SchlaU-Werkstatt – für Migrationspädagogik" /></li>
+            </ul>
+        </section>
+
+        <section id="contact">
+            <h2>Kontakt</h2>
+            <h3>Meine Anschrift</h3>
+            <address class="vcard">
+                <span class="fn org"><strong>Creative Studio Daniel Wiegand</strong></span><br>
+                <span class="adr work street-address">Durlacher Allee 53</span><br>
+                <span class="adr work postal-code">76131</span> <span class="adr work locality">Karlsruhe</span><br>
+                <span class="adr work country-name">Deutschland</span>
+            </address>  
+            <p>Klicken Sie hier für Anfahrtsinformationen auf <a href="https://goo.gl/maps/gB5VgH1Awiy4LyU98" target="_blank" rel="nofollow noopener">Google Maps</a>.</p>
+
+            <h3>Meine Kontaktinfos</h3>
+            <address class="vcard">
+                <a href="tel:+491704706439" rel="noopener noreferrer" class="tel work voice"><svg class="icon icon-phone" aria-label="Phone"><use xlink:href="images/symbol-defs.svg#icon-phone"></use></svg> +49 170 4706439</a><br>
+                <a class="email" href="mailto:daniel@wiegand.io" target="_blank" rel="noopener noreferrer"><svg class="icon icon-envelope-o"><use xlink:href="images/symbol-defs.svg#icon-envelope-o"></use></svg> daniel@wiegand.io</a><br>
+                <a href="skype:wiegimania" rel="noopener nofollow">
+                        <svg class="icon icon-skype" aria-label="Daniel Wiegand on Skype"><use xlink:href="images/symbol-defs.svg#icon-skype"></use></svg> Skype
+                </a>
+            </address>  
+
+            <h3>Treffen Sie mich</h3>
+            <p>
+                Ich bin auf folgenden Veranstaltungen, um für gemeinsame Projekte neuen kreativen Input zu sammeln:
+            </p>
+            <ul>
+                <li><a href="http://webmontag.de/location/karlsruhe/index" target="_blank" rel="nofollow noopener"><strong>Webmontag.Talk</strong></a> am 03. Juni 2019 in Karlsruhe</li>
+                <li><a href="https://2019.europe.wordcamp.org/" target="_blank" rel="nofollow noopener"><strong>WordCamp Europe 2019</strong></a> vom 20. bis 22. Juni 2019 in Berlin</li>
+                <li><a href="https://www.meetup.com/de-DE/WordPress-Meetup-Karlsruhe/events/260510800/" target="_blank" rel="nofollow noopener"><strong>WordPress Meetup #24</strong></a> am 18. Juli 2019 in Karlsruhe</li>
+                <li><a href="https://www.meetup.com/de-DE/WordPress-Meetup-Karlsruhe/events/260510818/" target="_blank" rel="nofollow noopener"><strong>WordPress Meetup #25</strong></a> am 12. September 2019 in Karlsruhe</li>
+            </ul>
+            <p>
+                Aber auch unsere Region hat einiges zu bieten und so können Sie mich regelmäßig auf dem <a href="http://webmontag.de/location/karlsruhe/index" rel="nofollow noopener">Webmontag Karlsruhe (Co-Organistor)</a>, <a href="https://www.meetup.com/de-DE/WordPress-Meetup-Karlsruhe/" rel="nofollow noopener">WordPress Meetup Karlsruhe (Co-Organistor)</a> oder anderen Events in und um Karlsruhe treffen.
+            </p> 
         </section>
 
         <footer>
