608c382ea4c5165c0f560deb91dd358eb721a6a5
Daniel Wiegand <daniel@wiegand.io>
2019-05-28 12:57:26 +0200

    added som grid stuff

--- index.php
+++ index.php
@@ -128,31 +128,40 @@
 
         <section id="contact">
             <h2>Kontakt</h2>
-            <h3>Meine Anschrift</h3>
-            <address class="vcard">
-                <span class="fn org"><strong>Creative Studio Daniel Wiegand</strong></span><br>
-                <span class="adr work street-address">Durlacher Allee 53</span><br>
-                <span class="adr work postal-code">76131</span> <span class="adr work locality">Karlsruhe</span><br>
-                <span class="adr work country-name">Deutschland</span>
-            </address>  
-            <p>Anfahrtsinformationen auf <a href="https://goo.gl/maps/gB5VgH1Awiy4LyU98" target="_blank" rel="nofollow noopener">Google Maps</a>.</p>
 
-            <h3>Meine Kontaktinfos</h3>
-            <address class="vcard">
-                <a href="tel:+491704706439" rel="noopener noreferrer" class="tel work voice"><svg class="icon icon-phone" aria-label="Phone"><use xlink:href="images/symbol-defs.svg#icon-phone"></use></svg> +49 170 4706439</a><br>
-                <a class="email" href="mailto:daniel@wiegand.io" target="_blank" rel="noopener noreferrer"><svg class="icon icon-envelope-o"><use xlink:href="images/symbol-defs.svg#icon-envelope-o"></use></svg> daniel@wiegand.io</a><br>
-                <a href="skype:wiegimania" rel="noopener nofollow"><svg class="icon icon-skype" aria-label="Daniel Wiegand on Skype"><use xlink:href="images/symbol-defs.svg#icon-skype"></use></svg> Skype</a>
-            </address>  
-
-            <h3>Treffen Sie mich</h3>
-            <p>
-                Ich bin auf folgenden Veranstaltungen, um für gemeinsame Projekte neuen kreativen Input zu sammeln:
-            </p>
-            <ul>
-                <li><a href="http://webmontag.de/location/karlsruhe/index" target="_blank" rel="nofollow noopener"><svg class="icon icon-external-link" aria-label="externer Link"><use xlink:href="images/symbol-defs.svg#icon-external-link"></use></svg> <strong>Webmontag.Talk</strong></a> am 03. Juni 2019 in Karlsruhe</li>
-                <li><a href="https://2019.europe.wordcamp.org/" target="_blank" rel="nofollow noopener"><svg class="icon icon-external-link" aria-label="externer Link"><use xlink:href="images/symbol-defs.svg#icon-external-link"></use></svg> <strong>WordCamp Europe 2019</strong></a> vom 20. bis 22. Juni 2019 in Berlin</li>
-                <li><a href="https://www.meetup.com/de-DE/WordPress-Meetup-Karlsruhe/events/260510800/" target="_blank" rel="nofollow noopener"><svg class="icon icon-external-link" aria-label="externer Link"><use xlink:href="images/symbol-defs.svg#icon-external-link"></use></svg> <strong>WordPress Meetup #24</strong></a> am 18. Juli 2019 in Karlsruhe</li>
-                <li><a href="https://www.meetup.com/de-DE/WordPress-Meetup-Karlsruhe/events/260510818/" target="_blank" rel="nofollow noopener"><svg class="icon icon-external-link" aria-label="externer Link"><use xlink:href="images/symbol-defs.svg#icon-external-link"></use></svg> <strong>WordPress Meetup #25</strong></a> am 12. September 2019 in Karlsruhe</li>
+            <ul class="card">
+                <li>
+                    <h3>Meine Anschrift</h3>
+                    <address class="vcard">
+                        <span class="fn org"><strong>Creative Studio Daniel Wiegand</strong></span><br>
+                        <span class="adr work street-address">Durlacher Allee 53</span><br>
+                        <span class="adr work postal-code">76131</span> <span class="adr work locality">Karlsruhe</span><br>
+                        <span class="adr work country-name">Deutschland</span>
+                    </address>  
+                    <p>Anfahrtsinformationen auf <a href="https://goo.gl/maps/gB5VgH1Awiy4LyU98" target="_blank" rel="nofollow noopener">Google Maps</a>.</p>
+                </li>
+                <li>
+                    <h3>Meine Kontaktinfos</h3>
+                    <address class="vcard">
+                        <a href="tel:+491704706439" rel="noopener noreferrer" class="tel work voice"><svg class="icon icon-phone" aria-label="Phone"><use xlink:href="images/symbol-defs.svg#icon-phone"></use></svg> +49 170 4706439</a><br>
+                        <a class="email" href="mailto:daniel@wiegand.io" target="_blank" rel="noopener noreferrer"><svg class="icon icon-envelope-o"><use xlink:href="images/symbol-defs.svg#icon-envelope-o"></use></svg> daniel@wiegand.io</a><br>
+                        <a href="skype:wiegimania" rel="noopener nofollow"><svg class="icon icon-skype" aria-label="Daniel Wiegand on Skype"><use xlink:href="images/symbol-defs.svg#icon-skype"></use></svg> Skype</a><br>
+                        <a href="https://www.linkedin.com/in/wiegimania/" target="_blank" rel="noopener nofollow"><svg class="icon icon-linkedin" aria-label="Daniel Wiegand on LinkedIn"><use xlink:href="images/symbol-defs.svg#icon-linkedin"></use></svg> LinkedIn</a><br>
+                        <a href="https://twitter.com/wiegimania/" target="_blank" rel="noopener nofollow"><svg class="icon icon-twitter" aria-label="Daniel Wiegand on Twitter"><use xlink:href="images/symbol-defs.svg#icon-twitter"></use></svg>  Twitter</a>
+                    </address>  
+                </li>
+                <li>
+                    <h3>Treffen Sie mich</h3>
+                    <p>
+                        Ich bin auf folgenden Veranstaltungen, um für gemeinsame Projekte neuen kreativen Input zu sammeln:
+                    </p>
+                    <ul>
+                        <li><a href="http://webmontag.de/location/karlsruhe/index" target="_blank" rel="nofollow noopener"><svg class="icon icon-external-link" aria-label="externer Link"><use xlink:href="images/symbol-defs.svg#icon-external-link"></use></svg> <strong>Webmontag.Talk</strong></a> (03.06.2019)</li>
+                        <li><a href="https://2019.europe.wordcamp.org/" target="_blank" rel="nofollow noopener"><svg class="icon icon-external-link" aria-label="externer Link"><use xlink:href="images/symbol-defs.svg#icon-external-link"></use></svg> <strong>WordCamp Europe 2019</strong></a> (20.06 - 22.06.2019)</li>
+                        <li><a href="https://www.meetup.com/de-DE/WordPress-Meetup-Karlsruhe/events/260510800/" target="_blank" rel="nofollow noopener"><svg class="icon icon-external-link" aria-label="externer Link"><use xlink:href="images/symbol-defs.svg#icon-external-link"></use></svg> <strong>WordPress Meetup #24</strong></a> (18.07.2019)</li>
+                        <li><a href="https://www.meetup.com/de-DE/WordPress-Meetup-Karlsruhe/events/260510818/" target="_blank" rel="nofollow noopener"><svg class="icon icon-external-link" aria-label="externer Link"><use xlink:href="images/symbol-defs.svg#icon-external-link"></use></svg> <strong>WordPress Meetup #25</strong></a> (12.09.2019)</li>
+                    </ul>
+                </li>
             </ul>
             <p>
                 Aber auch unsere Region hat einiges zu bieten und so können Sie mich regelmäßig auf dem <a href="http://webmontag.de/location/karlsruhe/index" rel="nofollow noopener">Webmontag Karlsruhe (Co-Organistor)</a>, <a href="https://www.meetup.com/de-DE/WordPress-Meetup-Karlsruhe/" rel="nofollow noopener">WordPress Meetup Karlsruhe (Co-Organistor)</a> oder anderen Events in und um Karlsruhe treffen.
