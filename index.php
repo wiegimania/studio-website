c4b78e5073c2c5dcb38c1c42ce3bbee42785c731
Daniel Wiegand <daniel@wiegand.io>
2020-05-13 14:22:50 +0200

    typo changes

--- index.php
+++ index.php
@@ -223,11 +223,11 @@
                 </li>
                 <li>
                     <figure>
-                        <img src="images/Markus-Knaup-Chrono24.png" alt="Markus Knaup (Head of Technology) Chrono24" />
+                        <img src="images/Markus-Knaup-Chrono24.png" alt="Markus Knaup (Head of Technology) Chrono24 GmbH" />
                         <blockquote>
                             „Mit seinem breitgefächerten Wissen und seinen pragmatischen Ansätzen unterstützt uns Daniel seit langer Zeit in unserem reichweitenstarken Magazin. Er hat dort eine tragfähige Headless-Lösung umgesetzt, die die Vorteile der WordPress- und Java-Welt vereint.“
                             <footer>
-                                — <cite class="author">Markus Knaup (Head of Technology)</cite> <cite class="company"><a href="https://www.chrono24.com/" target="_blank" rel="nofollow noopener">Chrono24</a></cite>
+                                — <cite class="author">Markus Knaup (Head of Technology)</cite> <cite class="company"><a href="https://www.chrono24.com/" target="_blank" rel="nofollow noopener">Chrono24 GmbH</a></cite>
                             </footer>
                         </blockquote>
                     </figure>
